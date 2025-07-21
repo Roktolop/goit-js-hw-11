8bb6304ba7e172434192e5c1f99c3c7afd35a353
Volodimir <sanin.vova200@gmail.com>
2025-07-21 22:03:22 +0300

    make photo searching and replace

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,42 @@
 `use strict`;
 
 import axios from 'axios';
+import iziToast from "izitoast";
 
-export function getImagesByQuery(query) {
-  axios.get(`https://pixabay.com/api/${query}`)
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
+const API_KEY = "51409693-2d1545100387263aa5c533cc2";
+
+export async function getImagesByQuery(query) {
+  try {
+    const response = await axios(`https://pixabay.com/api/`, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true
+      }
+    });
+
+    const images = response.data.hits;
+
+    if (images.length === 0) {
+      iziToast.error({
+        message: 'Sorry, no images match your search query. Please try again.',
+        position: 'topRight',
+      });
+      return []; 
+    }
+
+    return images; 
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: error.message,
+      position: 'topRight'
+    });
+    return []; 
+  }
 }
+
 
 
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,73 @@
 `use strict`;
 
 import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
-import {getImagesByQuery} from "./pixabay-api";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { getImagesByQuery } from "./pixabay-api.js";
+
+const form = document.querySelector(".form");
+const input = document.querySelector('input[name="search-text"]');
+const gallery = document.querySelector(".gallery");
+
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
 
-const galleryContainer = document.querySelector(".gallery");
+form.addEventListener("submit", onFormSubmit);
+
+async function onFormSubmit(event) {
+  event.preventDefault();
+
+  clearGallery();
+  
+  const query = input.value.trim();;
+  if (!query) return;
+
+  const images = await getImagesByQuery(query); 
+  console.log(images);
+  
+  if (images.length > 0) {
+    createGallery(images); 
+  }
+
+
+}
 
 //murkup create
 
 export function createGallery(images) {
     const markup = images
-        .map(({ webformatURL, largeImageURL, tags }) => `
-        <a class="gallery__item" href="${largeImageURL}">
-          <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <a class="gallery-item" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+          <ul class="photo-list">
+            <li class="photo-item">
+              <p>Likes ${likes}</p>
+            </li>
+            <li class="photo-item">
+              <p>Views ${views}</p>
+            </li>
+            <li class="photo-item">
+              <p>Comments ${comments}</p>
+            </li>
+            <li class="photo-item">
+              <p>Downloads ${downloads}</p>
+            </li>
+          </ul>
         </a>
       `
         )
         .join("");
     
-    galleryContainer.insertAdjacentHTML("beforeend", markup);
+    gallery.insertAdjacentHTML("beforeend", markup);
     lightbox.refresh();
 }
 
 //clear gallery
 
 export function clearGallery() {
-  const gallery = document.querySelector('.gallery');
   gallery.innerHTML = '';
 }
 
@@ -43,5 +80,5 @@ export function showLoader() {
 
 export function hideLoader() {
     const loader = document.querySelector('.loader');
-    loader.classList.remove('');
+    loader.classList.remove('is-visible');
 }
